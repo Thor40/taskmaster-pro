1d557fe9c76f9abb1667347854eff37a8cb797cb
Thor40 <nicholas.kosik@gmail.com>
2020-04-15 14:28:25 -0500

    drag and drop/delete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
 var tasks = {};
 
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+      ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
 
 $(".card .list-group").sortable({
   connectWith: $(".card .list-group"),
@@ -19,13 +32,37 @@ $(".card .list-group").sortable({
     console.log("out", event.target);
   },
   update: function(event) {
+  // array to store the task data in
+  var tempArr = [];
+
     // loop over current set of children in sortable list
     $(this).children().each(function() {
-      console.log($(this).children());
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
     });
+    // trim down list's ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
   }
 });
-
 
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
@@ -213,5 +250,3 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
-
-
